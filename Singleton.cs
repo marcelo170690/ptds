cfd501935590a0f88fe0b6e2fd1785f982f93d36
Alan Mercado <alan.droids@gmail.com>
2017-07-08 19:35:16 -0400

    Implemantacion de Singleton
    Para el control de una unica instancia en el programa

---
+++ Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace softchape
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+        private static int lvl { get; set; }
+
+        private Singleton() { }
+
+        public static Singleton getInstace()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+    }
+}
