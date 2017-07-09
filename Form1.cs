52e5382348bc9cebf29a4470673ea7b7e0b275ec
marcelo170690 <marcelo170690@gmail.com>
2017-07-08 22:04:36 -0400

    cambie la bd

--- Form1.cs
+++ Form1.cs
@@ -9,24 +9,19 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using softchape.dstablasTableAdapters;
 
-namespace softchape
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace softchape{
+    public partial class Form1 : Form{
+        public Form1(){
             InitializeComponent();
         }
         dstablas ds = new dstablas();
         TAcliente tac = new TAcliente();
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e){
             tac.Fill(ds.cliente);
             dataGridView1.DataSource = ds.cliente;
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
+        private void label1_Click(object sender, EventArgs e){
 
         }
 
